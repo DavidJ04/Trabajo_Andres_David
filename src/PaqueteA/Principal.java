20bc008fd0fb794c9d68c24821ba52aa48c7048b
DavidLS <DavidLS@LAPTOP-F803EC2K>
2022-03-04 20:03:53 -0600

    prueba 1 branch

--- src/PaqueteA/Principal.java
+++ src/PaqueteA/Principal.java
@@ -1,12 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+//David Leitón Salas
+//GitHub
+
 package PaqueteA;
 
 /**
  *
- * @author DavidLS
+ * @author DavidLS 
  */
 public class Principal {
 
