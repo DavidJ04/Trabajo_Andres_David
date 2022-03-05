3eb9cf957a8a8521be8e1863d9b30edcd8fc5305
DavidLS <DavidLS@LAPTOP-F803EC2K>
2022-03-04 21:21:50 -0600

    End

--- src/PaqueteA/Principal.java
+++ src/PaqueteA/Principal.java
@@ -3,9 +3,23 @@
 package PaqueteA;
 
 public class Principal {
-    
+
     public static void main(String[] args) {
         System.out.println("Hola a todos!");
+
+        Pet mascota = new Pet();
+        
+        Vehiculo elVehiculo = new Vehiculo();
+        
+         //Probando el objeto de la clase Vehiculo
+        elVehiculo.setPuertas(5);
+        int lasPuertas = elVehiculo.getPuertas();
+        System.out.println("Hay estas puertas: "+lasPuertas);
+        
+        //Probando el objeto de la clase Pet
+        String gatoName= "pusungo";
+        mascota.setGato(gatoName);        
+        System.out.println("Este es el nuevo nombre: " + gatoName);
     }
-    
+
 }
