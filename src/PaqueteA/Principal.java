d6b34c37f3f6f015ea56a70c7082b58ec972a5fb
DavidLS <DavidLS@LAPTOP-F803EC2K>
2022-03-04 19:43:20 -0600

    prueba 1.1

--- src/PaqueteA/Principal.java
+++ src/PaqueteA/Principal.java
@@ -11,7 +11,7 @@ package PaqueteA;
 public class Principal {
 
     /**
-     * @param args the command line arguments
+     *  @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
